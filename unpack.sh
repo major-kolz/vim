de9b8ec29dc34bf52009c21a956a1437969ccbed
major-kolz <major.kolz@gmail.com>
2016-04-24 15:27:23 +0300

    improve installing

--- unpack.sh
+++ unpack.sh
@@ -3,9 +3,15 @@
 # Configure GVim (installation or updating) by soft links 
 # (original is in directory of this repo)
 
-current=$HOME/prog/vim
-#mkdir -p $HOME/.vim
+current=`dirname $(realpath $0)`
+mkdir -p $HOME/.vim
 vdir=$HOME/.vim/
+
+installer="apt-get install"
+if command -v pacman 2> /dev/null
+then
+	installer="pacman --needed -S"
+fi
 
 ln -s "$current/.vimrc" -t "$HOME"
 ln -s "$current/ftplugin" -t "$vdir"
@@ -14,12 +20,12 @@ ln -s "$current/spell" -t "$vdir"
 ln -s "$current/syntax" -t "$vdir"
 ln -s "$current/snippets" -t "$vdir"
 
-#if [ ! -f "$vdir/autoload/plug.vim" ]; then
-#	if [ ! hash curl 2> /dev/null ]; then
-#		notyfi-send "--expire-time=3000" "Vim configuration" "Please install 'curl'"
-#		sudo -E apt-get install curl
-#	fi
-#	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.github.com/junegunn/vim-plug/master/plug.vim
-#fi
+if [ ! -f "$vdir/autoload/plug.vim" ]; then
+	if [ ! hash curl 2> /dev/null ]; then
+		notify-send "--expire-time=3000" "Vim configuration" "Please install 'curl'"
+		sudo -A $installer curl
+	fi
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.github.com/junegunn/vim-plug/master/plug.vim
+fi
 
-#notyfi-send "--expire-time=3000" "Vim configuration" "Done"
+notify-send "--expire-time=3000" "Vim configuration" "Done. Download plugins"
